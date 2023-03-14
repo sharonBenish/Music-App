0aebaad60b33402443075e503c719e646c20566d
MAC <sbenishwilliams@gmail.com>
2023-03-15 00:06:40 +0100

    build: tailwind config setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        'deepp':'hsl(209, 23%, 22%)',
+      }
+    },
+  },
+  plugins: [],
+}
